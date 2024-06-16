65bc836bc26a81ecf752ce5ce23297ff49890766
Cheee10 <152449727+Cheee10@users.noreply.github.com>
2024-06-16 09:30:08 +0800

    Update home.py

--- home.py
+++ home.py
@@ -6,11 +6,14 @@ show_pages(
     [   
         Page("home.py",),
         Section("Applications"),
-        Page("pages/crop_recom_streamlitapp.py", "Prediction", "1️⃣", in_section=True),
+        Page("pages/accuracy.py", "Prediction", "1️⃣", in_section=True),
         Page("pages/basic_sentiment_analyzer.py", "Sentiment Analysis", "2️⃣", in_section=True),
-        Page("pages/activity_endterm_4.py", "Image Classification 1 (Sky Condition)", "3️⃣", in_section=True),
-        Page("pages/img_classification_lettuce_diseases.py", "Image Classification 2 (Lettuce Diseaes)", "4️⃣", in_section=True),
+        Page("pages/activity_endterm_4.py", "Image Classification", "3️⃣", in_section=True),
 
+
+        Section("Source Codes"),
+        Page("pages/sentiment_src.py", "Sentiment Analysis Source", "2️⃣", in_section=True),
+        Page("pages/image_classification_src.py", "Image Classification Source", "3️⃣", in_section=True),
 ]
 )
 
@@ -65,9 +68,36 @@ Enter your name.
 Describe how you are feeling today.
 Click the "Analyze Sentiment" button to receive feedback.
 Feel free to express yourself and see how the Sentiment Analyzer responds!
-            
-### ⭐ Star the project on Github  <iframe src="https://ghbtns.com/github-btn.html?user=koalatech&repo=streamlit_web_app&type=star&count=true"  width="150" height="20" title="GitHub"></iframe>   
+   
 """, unsafe_allow_html=True)
+
+st.image("./food_prediction.png")
+st.markdown("""
+This web application leverages a machine learning model to classify uploaded images based on food categories. Using a pre-trained model, it analyzes features extracted from images to predict and display the category of food depicted.
+
+How it Works:
+
+Input: Users upload an image containing food items using the sidebar file uploader.
+
+Analysis: The application extracts visual features from the uploaded image using a deep learning model.
+
+Classification: The extracted features are processed through a trained classifier, which predicts the category of food in the image.
+
+Output: The predicted food category is displayed alongside the uploaded image, providing instant classification results.
+
+Key Features:
+
+Efficient Classification: Utilizes image embeddings for accurate food category prediction.
+User-friendly Interface: Simple file upload interface with real-time classification feedback.
+Scalable: Handles image uploads up to 5MB in size for comprehensive food image analysis.
+Try it Out:
+
+Upload: Select an image file containing food items.
+Predict: Click the "Predict Category" button to see the predicted food category.
+Explore: Explore various food images and see how accurately they are classified!
+Explore the world of food through the lens of machine learning with our Image Classification for Food web application!
+""", unsafe_allow_html=True)
+
 
 hide_streamlit_style = """
 <style>
