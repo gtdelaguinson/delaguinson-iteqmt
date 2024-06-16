8cf6730b66c646994197ce5abc6196a844e45497
gtdelaguinson <gtdelaguinson.chmsu@gmail.com>
2024-06-17 00:04:25 +0800

    Update sentiment_src.py

--- pages/sentiment_src.py
+++ pages/sentiment_src.py
@@ -1,35 +1,34 @@
 import streamlit as st
-
+import pandas as pd
+import pickle
+from nltk.corpus import names
 
 st.header('Simple Sentiment Analyzer App')
 st.subheader('This python code is implemented for Streamlit')
 st.code('''
-        import streamlit as st
-        import pandas as pd
-        import pickle
-        from nltk.corpus import names
+import streamlit as st
+import pandas as pd
+import pickle
+from nltk.corpus import names
 
-        st.title("A Simple Sentiment Analyzer")
-        message = st.text_input("Tell me what you feel today: ")
+st.title("A Simple Sentiment Analyzer")
+message = st.text_input("Tell me what you feel today: ")
 
-        # Load the trained Naive Bayes classifier from the saved file
-        filename = 'pages/sentimentAnalyzerTest_model.sav'
-        loaded_model = pickle.load(open(filename, 'rb'))
+# Load the trained Naive Bayes classifier from the saved file
+filename = 'pages/sentimentAnalyzerTest_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
 
-        # Define features (words) and their corresponding labels (positive/negative)
-        def word_features(words):
-            return dict([(word, True) for word in words])
+# Define features (words) and their corresponding labels (positive/negative)
+def word_features(words):
+    return dict([(word, True) for word in words])
 
-        message_tone = loaded_model.classify(word_features(message.split()))
+def sayFeeling():
+    message_tone = loaded_model.classify(word_features(message.split()))
+    # Classify the sentiment
+    if message_tone == 'positive':
+        st.write("this is :smile:")
+    else:
+        st.write("this is :disappointed:")
 
-        # make a function for your button click
-
-        def sayFeeling():
-            # Classify the sentiment
-            if message_tone == 'positive':
-                st.write("this is :smile:")
-            else:
-                st.write("this is :disappointed:")
-                
-        st.button('Say it', on_click=sayFeeling)
-    ''')
+st.button('Say it', on_click=sayFeeling)
+''')
