25a4dd859116a1b7d2950a916ef4b72afc4f8e7f
gtdelaguinson <gtdelaguinson.chmsu@gmail.com>
2024-06-17 00:12:51 +0800

    Update crop_src.py

--- pages/crop_src.py
+++ pages/crop_src.py
@@ -4,30 +4,37 @@ import streamlit as st
 st.header('Crop Recommendation App')
 st.subheader('This model was trained using a dataset')
 st.code('''
-        import streamlit as st
-        import pandas as pd
-        import pickle
-        from nltk.corpus import names
+        #Notes
+# do a "pip install streamlit" first 
+#to run on terminal issue this command
+# python -m streamlit run streamlit_test.py
 
-        # Load the trained Naive Bayes classifier from the saved file
-        filename = 'pages/crop_recom_model.sav'
-        loaded_model = pickle.load(open(filename, 'rb'))
+import streamlit as st
+import pandas as pd
+import pickle
+from nltk.corpus import names
 
-        # # Use the model to make predictions
-        def predict_crop():
-            st.text("The crop is " + crop_name)
-            return
-                
-        st.title("Crop Recommendation Predictor :smile:")
-        st.subheader("Enter a set of NPK levels to determine what crop best fits:")
-        n_input = st.slider("Nitrogen: ",0,500)
-        p_input = st.slider("Phosphorus: ",0,500)
-        k_input = st.slider("Potassium: ",0,500)
-        if n_input == 0 & p_input == 0 & k_input == 0:
-            crop_name = ""
-        else:
-            crop_name = loaded_model.predict([[pd.to_numeric(n_input),pd.to_numeric(p_input),pd.to_numeric(k_input)]])
+# Load the trained Naive Bayes classifier from the saved file
+filename = 'pages/crop_recom_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
 
-        st.text("The crop suitable for this NPK level:")
-        st.text_area(label ="",value=crop_name, height =100)
-    ''')
+# # Use the model to make predictions
+@st.cache_data 
+def predict_crop():
+    st.text("The crop is " + crop_name)
+    return
+           
+st.title("Crop Recommendation Predictor :smile:")
+st.subheader("Enter a set of NPK levels to determine what crop best fits:")
+n_input = st.slider("Nitrogen: ",0,500)
+p_input = st.slider("Phosphorus: ",0,500)
+k_input = st.slider("Potassium: ",0,500)
+if n_input == 0 & p_input == 0 & k_input == 0:
+    crop_name = ""
+else:
+    crop_name = loaded_model.predict([[pd.to_numeric(n_input),pd.to_numeric(p_input),pd.to_numeric(k_input)]])
+
+st.text("The crop suitable for this NPK level:")
+st.text_area(label ="",value=crop_name, height =100)
+# st.button('Predict', on_click=predict_crop
+''')
