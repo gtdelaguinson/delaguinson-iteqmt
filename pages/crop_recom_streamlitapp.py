84fcfb0885221180232b1655a79b12db69163277
Cheee10 <152449727+Cheee10@users.noreply.github.com>
2024-06-16 12:12:37 +0800

    Update crop_recom_streamlitapp.py

--- pages/crop_recom_streamlitapp.py
+++ pages/crop_recom_streamlitapp.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import pandas as pd
-import pickle
+import joblib
 
 # Load the trained model with error handling
 filename = 'pages/new_predict.sav'
 try:
-    with open(filename, 'rb') as file:
-        loaded_model = pickle.load(file)
+    loaded_model = joblib.load(filename)
 except FileNotFoundError:
     st.error(f"Model file not found: {filename}")
     st.stop()
@@ -17,7 +16,6 @@ except Exception as e:
 # Function to predict laptop brand
 def predict_brand(features):
     if loaded_model is not None:
-        # Assuming loaded_model is a classifier
         brand_name = loaded_model.predict([features])[0]
         st.success(f"The predicted brand is {brand_name}")
     else:
