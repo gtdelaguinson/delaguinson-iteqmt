a4fa43712a2356d74bd492d9829445b10c67cb03
gtdelaguinson <gtdelaguinson.chmsu@gmail.com>
2024-06-17 00:08:44 +0800

    Update basic_sentiment_analyzer.py

--- pages/basic_sentiment_analyzer.py
+++ pages/basic_sentiment_analyzer.py
@@ -1,34 +1,23 @@
-# do a "pip install streamlit" first 
-
 import streamlit as st
-import pandas as pd
-import pickle
-from nltk.corpus import names
 
-st.title("A Simple Sentiment Analyzer")
-message = st.text_input("Tell me what you feel today: ")
+st.title("Sentiment Analyzer")
+name = st.text_input("What's your name?")
+message = st.text_input("Tell me what you feel today:")
 
-# Load the trained Naive Bayes classifier from the saved file
-filename = 'pages/sentimentAnalyzerTest_model.sav'
-loaded_model = pickle.load(open(filename, 'rb'))
+positive_words = ['good', 'excited', 'happy', 'great', 'fantastic', 'wonderful']
+negative_words = ['bad', 'sad', 'angry', 'terrible', 'awful', 'miserable']
 
-# Define features (words) and their corresponding labels (positive/negative)
-# @st.cache_data 
-def word_features(words):
-    return dict([(word, True) for word in words])
-
-message_tone = loaded_model.classify(word_features(message.split()))
-
-# make a function for your button click
-# @st.cache_data 
 def sayFeeling():
-    # Classify the sentiment
-    if message_tone == 'positive':
-        st.write("this is :smile:")
+    if name and message:
+        st.write(f"Hi, {name}!")
+        words = message.lower().split()
+        if any(word in positive_words for word in words):
+            st.write("That's good! :smile:")
+        elif any(word in negative_words for word in words):
+            st.write("I hope you feel better soon. :disappointed:")
+        else:
+            st.write("Keep going! :neutral_face:")
     else:
-        st.write("this is :disappointed:")
-        
-st.button('Say it', on_click=sayFeeling)
+        st.write("Please enter both your name and how you feel today.")
 
-#to run on terminal issue this command
-# python -m streamlit run streamlit_test.py
+st.button('Say it', on_click=sayFeeling)
