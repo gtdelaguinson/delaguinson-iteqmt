f543236ddb9889b854b7184e8ff026c671d01b11
gtdelaguinson <gtdelaguinson.chmsu@gmail.com>
2024-06-17 00:25:17 +0800

    Update image_classification_src.py

--- pages/image_classification_src.py
+++ pages/image_classification_src.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.header('Image Classification App')
 st.subheader('This python code is implemented for Streamlit')
 st.code('''
-        import pickle
+import pickle
 from img2vec_pytorch import Img2Vec
 from PIL import Image
 import streamlit as st
@@ -22,7 +22,7 @@ img2vec = Img2Vec()
 
 
 ## Streamlit Web App Interface
-st.set_page_config(layout="wide", page_title="Image Classification for color")
+st.set_page_config(layout="wide", page_title="Image Classification for Color")
 
 st.write("## Let's try to see what color is in the image!")
 st.write(
@@ -33,14 +33,14 @@ st.sidebar.write("## Upload and download :gear:")
 MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 
 # Download the fixed image
-@st.cache_data 
+# @st.cache_data 
 def convert_image(img):
     buf = BytesIO()
     img.save(buf, format="jpg")
     byte_im = buf.getvalue()
     return byte_im
 
-@st.cache_data 
+# @st.cache_data 
 def fix_image(upload):
     image = Image.open(upload)
     col1.write("Image to be predicted :camera:")
