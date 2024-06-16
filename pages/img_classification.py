f5724b46881d3a29390e3afaee24c59b717410a5
Cheee10 <152449727+Cheee10@users.noreply.github.com>
2024-06-16 10:07:17 +0800

    Update img_classification.py

--- pages/img_classification.py
+++ pages/img_classification.py
@@ -1,42 +1,60 @@
 import pickle
+from img2vec_pytorch import Img2Vec
+from PIL import Image
+import streamlit as st
+# from rembg import remove
 from PIL import Image
 from io import BytesIO
-from img2vec_pytorch import Img2Vec
-import streamlit as st
+import base64
 
-st.set_page_config(layout="wide", page_title="Image Classification for Foods")
 
-st.write("## Image Classification Model for Foods")
-st.write(":grin: Predicting food categories from uploaded images :grin:")
-st.sidebar.write("## Upload and download :gear:")
-
-MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
-
-# Load the pre-trained model
-with open('/pages/animals.p', 'rb') as f:
+with open('pages/animals.p', 'rb') as f:
     model = pickle.load(f)
 
 img2vec = Img2Vec()
 
-@st.cache
-def classify_image(upload):
-    image = Image.open(upload)
-    st.write("### Image to be predicted:")
-    st.image(image)
 
-    st.write("### Predicted Category:")
-    features = img2vec.get_vec(image)
+
+## Streamlit Web App Interface
+st.set_page_config(layout="wide", page_title="Image Classification for Food")
+st.write("### Image Classification for Food!!")
+
+st.sidebar.write("## Upload and download :gear:")
+
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
+
+# Download the fixed image
+# @st.cache_data 
+def convert_image(img):
+    buf = BytesIO()
+    img.save(buf, format="jpg")
+    byte_im = buf.getvalue()
+    return byte_im
+
+# @st.cache_data 
+def fix_image(upload):
+    image = Image.open(upload)
+    col1.write("Image to be predicted :camera:")
+    col1.image(image)
+
+    # fixed = remove(image)
+    col2.write("Category :wrench:")
+    # image_path = './example/rain.jpeg'
+    img = Image.open(my_upload)
+    features = img2vec.get_vec(img)
     pred = model.predict([features])
 
-    st.header(pred[0])  # Display the predicted category
+    # print(pred)
+    col2.header(pred)
+    # st.sidebar.markdown("\n")
+    # st.sidebar.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
+
 
 col1, col2 = st.columns(2)
-my_upload = col1.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
 if my_upload is not None:
     if my_upload.size > MAX_FILE_SIZE:
         st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
     else:
-        classify_image(my_upload)
-else:
-    st.write("Upload an image to classify it.")
+        fix_image(upload=my_upload)
